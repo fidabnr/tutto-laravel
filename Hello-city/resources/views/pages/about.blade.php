4fa2d6df4c144562e3c399908036f3d72a5e6a47
fida bnr <fidabnr5@gmail.com>
2022-06-16 14:59:39 +0100

    some fixes

--- Hello-city/resources/views/pages/about.blade.php
+++ Hello-city/resources/views/pages/about.blade.php
@@ -4,18 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Abou us | Hello City</title>
+        <title>Hello City</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body >
-        <h1>Hello From Tunisia !!</h1>
-        <p>it's currently {{date('h:i A')}} </p>
+        <p>Built by &hearts; Fida Bannour</p>
+     
+        <p><a href="/">Revenir à la page d'aceuil</a></p>
+
         <footer>
-            <p>&copy; copyright 2022 &middot; <a href="/about-us"> About Us</a></p>
-        </footer>
+            <p>&copy; copyright 2022 &middot; </p>
+        </footer>       
     </body>
 </html>
 
--- Hello-city/resources/views/pages/welcome.blade.php
+++ Hello-city/resources/views/pages/welcome.blade.php
@@ -4,20 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>Abou us | Hello City</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body >
-        <p>Built by &hearts; Fida Bannour</p>
-     
-        <p><a href="/">Revenir à la page d'aceuil</a></p>
-
+        <h1>Hello From Tunisia !!</h1>
+        <p>it's currently {{date('h:i A')}} </p>
         <footer>
-            <p>&copy; copyright 2022 &middot; <a href="/about-us"> About Us</a></p>
-        </footer>       
+            <p>&copy; copyright 2022 &middot; 
+                <a href="/about-us"> About Us</a></p>
+        </footer>
     </body>
 </html>
 
