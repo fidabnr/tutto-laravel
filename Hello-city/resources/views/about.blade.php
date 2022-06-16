27d369f7ba8dce9ed34157f8e5bfdfb5ac0dac9e
fida bnr <fidabnr5@gmail.com>
2022-06-16 12:32:12 +0100

    firstttt

---
+++ Hello-city/resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello City</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body >
+        <p>Built by &hearts; Fida Bannour</p>
+     
+        <p><a href="/">Revenir à la page d'aceuil</a></p>
+
+        <footer>
+            <p>&copy; copyright 2022 &middot; <a href="/about-us"> About Us</a></p>
+        </footer>       
+    </body>
+</html>
+
+
