3c4928927b473a83cba30567e664ab21d5037f65
fida bnr <fidabnr5@gmail.com>
2022-06-17 09:25:11 +0100

    remove duplication by adding base layout

--- Hello-city/resources/views/pages/about.blade.php
+++ Hello-city/resources/views/pages/about.blade.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Hello City</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body >
+    @section('content')
         <p>Built by &hearts; Fida Bannour</p>
      
         <p><a href="/">Revenir à la page d'aceuil</a></p>
 
-        <footer>
-            <p>&copy; copyright 2022 &middot; </p>
-        </footer>       
-    </body>
-</html>
+            
+    @endsection
 
 
