8a5d580169766e42a27339f8751485bffcdd3b69
fida bnr <fidabnr5@gmail.com>
2022-06-20 08:52:23 +0100

    add images

--- Hello-city/resources/views/pages/about.blade.php
+++ Hello-city/resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
  @section('title' , 'About us | '.env('APP_NAME'))
 
     @section('content')
+
+    <img src="/img/Moon.png"alt="Tunisia Flag">
         <p>Built by &hearts; Fida Bannour</p>
      
         <p><a href="/">Revenir à la page d'aceuil</a></p>
--- Hello-city/resources/views/pages/welcome.blade.php
+++ Hello-city/resources/views/pages/welcome.blade.php
@@ -1,6 +1,8 @@
 @extends('base')
 
     @section('content')
+
+    <img src="/img/FlagTunisia.png"alt="Tunisia Flag">
     
         <h1>Hello From Tunisia !!</h1>
         <p>it's currently {{date('h:i A')}} </p>
